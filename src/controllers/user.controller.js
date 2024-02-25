e830479e369b35207800101af8eab5d718fcfd6f
Toursafe <vc76it10@vedascollege.edu.np>
2024-02-25 18:03:23 +0545

    subscription method

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -151,7 +151,7 @@ const logoutUser = asyncHandler(async(req, res) => {
 const refreshAccessToken = asyncHandler(async(req, res) =>{
     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-    if (incomingRefreshToken){
+    if (!incomingRefreshToken){
         throw new ApiError(401,"Unauthorized request")
     
     }
@@ -193,11 +193,12 @@ const refreshAccessToken = asyncHandler(async(req, res) =>{
         throw new ApiError(401, error?.message || "Invalid refresh token")
     }
 
-    
+
 } )
 export {
    registerUser ,
    loginUser,
-   logoutUser
+   logoutUser,
+   refreshAccessToken
 }
 
